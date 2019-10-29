d0e7bda1e9a815dee64289983667b9ca767de3d2
peterwatt <pjwatt@gmail.com>
2019-10-29 17:23:02 +1100

    Improve output

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,13 @@ aws cloudformation deploy \
     --capabilities CAPABILITY_IAM
 
 export ADDRESS=$(aws cloudformation describe-stacks --stack-name TestPackage --output text --query "Stacks[0].Outputs[?OutputKey=='Url'].{Url:OutputValue}")
+
+echo $'\n'
 echo $ADDRESS
-echo
+echo $'\n'
 curl $ADDRESS 
-echo
+echo $'\n'
 curl $ADDRESS 
-echo
+echo $'\n'
 curl $ADDRESS
-echo
+echo $'\n'
