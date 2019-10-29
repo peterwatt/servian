d468e2c8f076ae6383ab57bc27384214cb27e67d
peterwatt <pjwatt@gmail.com>
2019-10-29 17:13:13 +1100

    Make simpler

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,15 @@
-aws cloudformation package \
-    --template-file servian.yml \
-    --s3-bucket peterwatt \
-    --output-template-file servian-packaged.yaml
-
 aws cloudformation deploy \
-    --region 'ap-southeast-2' \
-    --template-file servian-packaged.yaml \
-    --stack-name 'TestPackage' \
+    --region ap-southeast-2 \
+    --template-file servian.yml \
+    --stack-name TestPackage \
     --capabilities CAPABILITY_IAM
 
 export ADDRESS=$(aws cloudformation describe-stacks --stack-name TestPackage --output text --query "Stacks[0].Outputs[?OutputKey=='Url'].{Url:OutputValue}")
-
+echo $ADDRESS
+echo
+curl $ADDRESS 
+echo
+curl $ADDRESS 
+echo
 curl $ADDRESS
-curl $ADDRESS
-curl $ADDRESS
+echo
